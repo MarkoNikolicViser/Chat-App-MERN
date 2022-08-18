cb40bfef7faf21afadc78b9f87fd463be4930839
markonikolicviser <markonikolicviser@gmail.com>
2022-08-18 23:41:03 +0200

    created and tested endpoint for creating group chats

--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { fetchChats, chatGet } = require('../controllers/chatControllers');
+const {
+  fetchChats,
+  chatGet,
+  createGroup,
+} = require('../controllers/chatControllers');
 const { protect } = require('../middleware/authMidleware');
 
 router.get('/get', protect, chatGet);
-router.post('/getallchats', fetchChats);
+router.post('/getallchats', protect, fetchChats);
+router.post('/creategroup', protect, createGroup);
 
 module.exports = router;
