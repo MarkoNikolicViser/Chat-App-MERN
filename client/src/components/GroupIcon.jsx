5636d5b063e6cc07464f403939ee6ce571dbce7d
markonikolicviser <markonikolicviser@gmail.com>
2022-09-26 23:30:00 +0200

    added group icon compoenent

--- client/src/components/GroupIcon.jsx
+++ client/src/components/GroupIcon.jsx
@@ -3,13 +3,13 @@ import { Icon } from './Icon'
 
 export const GroupIcon = (prop) => {
   //const {arr}=prop
-  const arr=[1,2,3]
+  const arr=[1,2,3,4]
   return (
-    <>
+    <div className={'absolute'}>
       {arr.map((m,index)=>
-        <div key={index}>
-          <Icon />
+        <div className={`absolute left-${index+1}`} key={index}>
+          <Icon image={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'}/>
         </div>)}
-    </>
+    </div>
   )
 }
--- client/src/features/Home/LeftSection/components/ChatListElement.jsx
+++ client/src/features/Home/LeftSection/components/ChatListElement.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Icon } from '../../../../components'
+import { Icon,GroupIcon } from '../../../../components'
 import ctl from '@netlify/classnames-template-literals'
 import { DateFormater } from '../../../../Helper/Formating'
 
 export const ChatListElement = (props) => {
   const { setSelectedChat, data } = props
-  const {chatName,updatedAt,groupAdmin}=data
+  const {chatName,updatedAt,groupAdmin,isGroupChat}=data
   return (
     <div onClick={() => setSelectedChat(data?._id)} className={mainDivStyle}>
-      <Icon image={groupAdmin?.picture} />
+      {!isGroupChat?
+        <GroupIcon />:
+        <Icon image={groupAdmin?.picture} />}
       <div className="w-full flex flex-col justify-center">
         <div className="w-[95%] flex justify-between">
           <h1>{chatName}</h1>
@@ -21,6 +23,7 @@ export const ChatListElement = (props) => {
 }
 const mainDivStyle = ctl(`w-full
 flex
+relative
 hover:bg-blue-100
 hover:cursor-pointer
 rounded-lg
